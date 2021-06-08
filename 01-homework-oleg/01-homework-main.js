bd3ca4a8145ad756e085d8b8cf3a2d6948e90795
Tabletka <tabletka226@gmail.com>
2021-06-08 17:32:40 +0300

    fixed

--- 01-homework-oleg/01-homework-main.js
+++ 01-homework-oleg/01-homework-main.js
@@ -18,16 +18,14 @@ const max = Math.max(+getUserNumber2, +getUserNumber);
 
 const userChoice = confirm("Пропускати парні?");
 let sum = null;
-// let count = getUserNumber;
+
 
 
 for (let i = min; i <= max; i++) {
     if (userChoice && i % 2 == 0) {
-
         continue;
     }
     sum += i;
-
 }
 
 console.log(sum);
@@ -40,32 +38,4 @@ console.log(sum);
 
 
 
-
-// sum = sum + count;
-
-
-
-
-// let user = 1;
-// let usere2 = 10;
-
-
-// let sum = user + 2;
-// sum = sum + 3;
-// sum = sum + 4;
-// sum = sum + 5;
-// sum = sum + 6;
-// sum = sum + 7;
-// sum = sum + 8;
-// sum = sum + 9;
-// sum = sum + 10;
-// console.log(sum);
-
-
-//FUNCTIONS//
-
-// function getMaxDigit(...arr) {
-//     return Math.max(arr)
-// }
-// console.log(getMaxDigit(1234));
 
