ee33507f10b92daca7de73ed2b1e913ecc4a3bc9
Tabletka <tabletka226@gmail.com>
2021-06-10 18:44:13 +0300

    Fixed validation

--- 01-homework-oleg/01-homework-main.js
+++ 01-homework-oleg/01-homework-main.js
@@ -1,26 +1,19 @@
-let getUserNumber = +prompt('Введіть ціле число з якого почати рахувати');
+// N - Перше число користувача
+let N = +prompt('Введіть ціле число з якого почати рахувати');
 
-if (getUserNumber == 0) {
-    //Пропускаємо 0
-} else {
-    while (getUserNumber == "" || isNaN(getUserNumber) || !Number.isInteger(getUserNumber)) {
-        getUserNumber = +prompt('Не вірне перше число, введіть ціле число');
-    }
+while (N === "" || isNaN(N) || !Number.isInteger(N)) {
+    N = +prompt('Не вірне перше число, введіть ціле число');
 }
 
-let getUserNumber2 = +prompt('Введіть ціле число до якого рахувати');
+// М - друге число користувача
+let M = +prompt('Введіть ціле число до якого рахувати');
 
-if (getUserNumber2 == 0) {
-    //Пропускаємо 0
-} else {
-    while (getUserNumber2 == "" || isNaN(getUserNumber2) || !Number.isInteger(getUserNumber2)) {
-        getUserNumber2 = +prompt('Не вірне друге число, введіть ціле число');
-    }
+while (M === "" || isNaN(M) || !Number.isInteger(M)) {
+    M = +prompt('Не вірне друге число, введіть ціле число');
 }
 
-
-const min = Math.min(getUserNumber2, getUserNumber);
-const max = Math.max(getUserNumber2, getUserNumber);
+const min = Math.min(M, N);
+const max = Math.max(M, N);
 const skipEvenNumbers = confirm("Пропускати парні?");
 let sum = null;
 
